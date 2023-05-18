775fa2dcfbeff817dc5257f2a9e7cb36a2ab71fe
JosephChennattu123 <joch00001@uni-saarland.de>
2023-05-18 21:50:12 +0200

    hehe mirror horizontal done

--- src/image_edit.c
+++ src/image_edit.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 
+#include "structures.h"
 #include "util.h"
 
 void rotate_counterclockwise(image_t *img) {
@@ -15,9 +16,22 @@ void rotate_clockwise(image_t *img) {
 }
 
 void mirror_horizontal(image_t *img) {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
-}
+    int column = img->w;
+    int row = img->h;
+    pixel_t *mir_whore = malloc(row*column*sizeof(pixel_t));
+    for(int i = 0;i<row;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            mir_whore[((row*i)+(column-1-j))].r=img->img[(row*i)+j].r;
+            mir_whore[(row*i)+(column-1-j)].g=img->img[(row*i)+j].g;
+            mir_whore[(row*i)+(column-1-j)].b=img->img[(row*i)+j].b;
+        }
+    }
+    free(img->img);
+    img->img = mir_whore;
+
+    }
 
 void mirror_vertical(image_t *img) {
     NOT_IMPLEMENTED;
