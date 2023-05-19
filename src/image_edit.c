6a3024830b41896e2948922dc05582ec0a37553e
JosephChennattu123 <joch00001@uni-saarland.de>
2023-05-19 11:47:01 +0200

    counter clockwise

--- src/image_edit.c
+++ src/image_edit.c
@@ -6,9 +6,25 @@
 #include "util.h"
 
 void rotate_counterclockwise(image_t *img) {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
-}
+    int column = img->w;
+    int row = img->h;
+    int nc = row;
+    int nr = column;
+    pixel_t *mir_whore = malloc(row*column*sizeof(pixel_t));
+    for(int i = 0;i<nr;i++)
+    {
+        for(int j=0;j<nc;j++)
+        {
+            mir_whore[(nr*i)+j].r=img->img[(row*(j))+(column-i-1)].r;
+            mir_whore[(nr*i)+j].g=img->img[(row*(j))+(column-i-1)].g;
+            mir_whore[(nr*i)+j].b=img->img[(row*(j))+(column-i-1)].b;
+        }
+    }
+    free(img->img);
+    img->h = nc;
+    img->w = nr;
+    img->img = mir_whore;
+   }
 
 void rotate_clockwise(image_t *img) {
     NOT_IMPLEMENTED;
@@ -34,8 +50,21 @@ void mirror_horizontal(image_t *img) {
     }
 
 void mirror_vertical(image_t *img) {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
+     int column = img->w;
+    int row = img->h;
+    pixel_t *mir_whorer = malloc(row*column*sizeof(pixel_t));
+    for(int i = 0;i<row;i++)
+    {
+        for(int j=0;j<column;j++)
+        {
+            mir_whorer[(row*(row-i-1))+(j)].r=img->img[(row*i)+j].r;
+            mir_whorer[(row*(row-i-1))+(j)].g=img->img[(row*i)+j].g;
+            mir_whorer[(row*(row-i-1))+(j)].b=img->img[(row*i)+j].b;
+        }
+    }
+    free(img->img);
+    img->img = mir_whorer;
+
 }
 
 void resize(image_t *img, int new_width, int new_height) {
